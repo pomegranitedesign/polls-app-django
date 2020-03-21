51b3c3028381b19f1f3d7f95d15c06ad3d75feed
Dmitriy "dmitriyshin.io" Shin <dimashin9@gmail.com>
2020-03-21 16:31:50 -0400

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/got_hacked/Desktop/Web/Python/1. Courses Projects/Polling App (Django Website)/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
